18f7050395a9d92a2e756ad03df12f19113c0910
Leonardo Pereira <leodilon@hotmail.com>
2020-04-24 11:49:17 -0300

    commit 24-04-2020

---
+++ src/config/loader.php
@@ -0,0 +1,38 @@
+<?php
+
+function loadModel($modelName) {
+    require_once(MODEL_PATH . "/{$modelName}.php");
+}
+
+function loadView($viewName, $params = array()) {
+
+    if(count($params) > 0) {
+        foreach($params as $key => $values) {
+            if(strlen($key) > 0) {
+                ${$key} = $values;
+            }
+        }
+    }
+
+    require_once(VIEW_PATH . "/{$viewName}.php");
+}
+
+function loadTemplateView($viewName, $params = array()) {
+
+    if(count($params) > 0) {
+        foreach($params as $key => $values) {
+            if(strlen($key) > 0) {
+                ${$key} = $values;
+            }
+        }
+    }
+
+    require_once(TEMPLATE_PATH . "/header.php");
+    require_once(TEMPLATE_PATH . "/left.php");
+    require_once(VIEW_PATH . "/{$viewName}.php");
+    require_once(TEMPLATE_PATH . "/footer.php");
+}
+
+function renderTitle($title, $subtitle, $icon = null) {
+    require_once(TEMPLATE_PATH . "/title.php");
+}
---
+++ src/views/day_records.php
@@ -0,0 +1,33 @@
+<main class="content">
+    <?php
+        renderTitle(
+            'Registrar Ponto',
+            'Mantenha seu ponto consistente',
+            'icofont-check-alt'
+        );
+        include(TEMPLATE_PATH . "/messages.php");
+    ?>
+
+    <div class="card">
+        <div class="card-header">
+            <h3><?= $today ?></h3>
+            <p class="mb-0">Os batimentos efetuados hoje</p>
+        </div>
+        <div class="card-body">
+            <div class="d-flex m-5 justify-content-around">
+                <span class="records">Entrada 1: ---</span>
+                <span class="records">Saida 1: ---</span>
+            </div>
+            <div class="d-flex m-5 justify-content-around">
+                <span class="records">Entrada 2: ---</span>
+                <span class="records">Saida 2: ---</span>
+            </div>
+        </div>
+        <div class="card-footer d-flex justify-content-center">
+            <a href="???" class="btn btn-success btn-lg">
+                <i class="icofont-check mr-1"></i>
+                Bater o Ponto
+            </a>
+        </div>
+    </div>
+</main>
